bf0029c338c79fd8b04c51d4b14eedb7e125fa06
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-02-04 18:39:20 +0100

    App Layout Component

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+
+
+const AppLayout = () => {
+  return (
+    <div>
+
+    </div>
+  )
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
