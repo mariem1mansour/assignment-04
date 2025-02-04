c9c92fbd492efce1bc2b19963591e808d4aa18e4
mariem1mansour <125161142+mariem1mansour@users.noreply.github.com>
2025-02-04 18:47:37 +0100

    create Header Component

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
 
-
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="Logo">
+        <img
+          src="https://i.pinimg.com/originals/08/3b/2f/083b2fe2646cd064e3a294bb716810f9.jpg"
+          alt="logo"
+        />
+      </div>
+      <div className="NavItems">
+        <ul className="NavItemsList">
+          <li className="NavItem">Home</li>
+          <li className="NavItem">About Us</li>
+          <li className="NavItem">Contact</li>
+          <li className="NavItem">Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const AppLayout = () => {
   return (
     <div>
-
+      <Header />
     </div>
-  )
+  );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
